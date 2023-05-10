f686d84c4caf67df2d9222ccb3df7dba46994976
JonesMertz <jonasmertz@gmail.com>
2023-05-11 00:48:41 +0200

    Alpha version treemap

--- 1 experiment/viz_gallery/script/assets/treemap_file.js
+++ 1 experiment/viz_gallery/script/assets/treemap_file.js
@@ -1,8 +1,24 @@
 var file = `name,parent,value
 Origin,,
-grp1,Origin,12
-grp2,Origin,23
-grp3,Origin,11
-grp4,Origin,40
-grp5,Origin,30
-grp6,Origin,25`;
+A Long-expected Party,Origin,90
+The Shadow of the Past,Origin,72
+Three is Company,Origin,54
+A Short Cut to Mushrooms,Origin,45
+A Conspiracy Unmasked,Origin,63
+The Old Forest,Origin,81
+In the House of Tom Bombadil,Origin,36
+Fog on the Barrow-downs,Origin,54
+At the Sign of the Prancing Pony,Origin,72
+Strider,Origin,63
+A Knife in the Dark,Origin,90
+Flight to the Ford,Origin,63
+Many Meetings,Origin,81
+The Council of Elrond,Origin,108
+The Ring Goes South,Origin,99
+A Journey in the Dark,Origin,117
+The Bridge of Khazad-dûm,Origin,90
+Lothlórien,Origin,81
+The Mirror of Galadriel,Origin,54
+Farewell to Lórien,Origin,72
+The Great River,Origin,90
+The Breaking of the Fellowship,Origin,135`;
