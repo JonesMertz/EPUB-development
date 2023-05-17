976e7ab70241080f7a420a96eeba2fe122d2180b
JonesMertz <jonasmertz@gmail.com>
2023-05-17 11:36:36 +0200

    Lord of the rings experiment

---
+++ 1 experiment/The_Fellowship_of_the_Ring_The_Lord_of_the_Rings_1_-_J_R_R_Tolkien-Old-Old/OEBPS/script/assets/treemap_file.js
@@ -0,0 +1,24 @@
+var file = `name,parent,value,color
+Origin,,,
+A Long-expected Party,Origin,90,#59504e
+The Shadow of the Past,Origin,72,#59504e
+Three is Company,Origin,54,#59504e
+A Short Cut to Mushrooms,Origin,45,#59504e
+A Conspiracy Unmasked,Origin,63,#59504e
+The Old Forest,Origin,81,#59504e
+In the House of Tom Bombadil,Origin,36,#59504e
+Fog on the Barrow-downs,Origin,54,#59504e
+At the Sign of the Prancing Pony,Origin,72,#59504e
+Strider,Origin,63,#59504e
+A Knife in the Dark,Origin,90,#59504e
+Flight to the Ford,Origin,63,#59504e
+Many Meetings,Origin,81,#59504e
+The Council of Elrond,Origin,108,#59504e
+The Ring Goes South,Origin,99,#59504e
+A Journey in the Dark,Origin,117,#59504e
+The Bridge of Khazad-dûm,Origin,90,#59504e
+Lothlórien,Origin,81,#59504e
+The Mirror of Galadriel,Origin,54,#59504e
+Farewell to Lórien,Origin,72,#59504e
+The Great River,Origin,90,#59504e
+The Breaking of the Fellowship,Origin,135,#59504e`;
