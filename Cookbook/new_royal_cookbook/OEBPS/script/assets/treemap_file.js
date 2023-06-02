5b7706e3c9282c6f96f782b03b0486bad2d832fd
JonesMertz <jonasmertz@gmail.com>
2023-06-03 01:23:03 +0200

    file cleanup

--- Cookbook/new_royal_cookbook/OEBPS/script/assets/treemap_file.js
+++
@@ -1,9 +0,0 @@
-var file = `name,parent,value,ingredient
-Origin,,,
-Albuminized Orange,Origin,42,Chocolate Apple
-Almonds Salted,Origin,42,Chocolate
-Angel Cake,Origin,42,Chocolate
-Apple Cake,Origin,42,Apple
-Apple Dressing,Origin,42,Apple Chocolate
-Apple Dumplings,Origin,42,Apple
-Apple Fritters,Origin,42,Apple`;
